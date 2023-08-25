77d5c900f2d7da9c3d7d3d84053a89ff732a7993
William McKeon <william.m@islanderintelligence.com>
2023-08-25 16:49:52 -0400

    made some small changes to the src folder

--- src/filter.py
+++ src/filter.py
@@ -1,9 +1,11 @@
-
+import requests
+from bs4 import BeautifulSoup as bs
+from datetime import datetime
 import pandas as pd
 def user_filter(dataset):
     User_filter = {"float shares":(input("enter the float value you want the stocks to have less then or equal to")),
     "Country":"United States",
-    "Sector":"",
+    "Sector":"Technology",
     "Industry":"",}
     user_dataset = {"Symbol":[], "Name":[],
     "Country":[],
@@ -46,4 +48,27 @@ def user_filter(dataset):
     user_dataset["Volume"] = vol
     user_dataset.dropna(inplace=True)
     user_dataset.sort_values(by=["Volume"],ascending=False,inplace=True)
+    article = []
+    published_today = []
+    for sym in user_dataset.Symbol:
+        try:
+            published_today.append(datetime.fromtimestamp(yf.Ticker(sym).news[0]["providerPublishTime"]).strftime('%Y-%m-%d %H:%M:%S').split(" ")[0]==datetime.today().strftime('%Y-%m-%d %H:%M:%S').split(" ")[0])
+        except:
+            published_today.append(False)
+        try:
+            # published_today.append(datetime.fromtimestamp(yf.Ticker(sym).news[0]["providerPublishTime"]).strftime('%Y-%m-%d %H:%M:%S').split(" ")[0]==datetime.today().strftime('%Y-%m-%d %H:%M:%S').split(" ")[0])
+            url = yf.Ticker(sym).news[0]["link"]
+            # print(url)
+            # print(sym)
+            data = requests.get(url)
+            data = bs(data.content)
+        
+            article.append(data.find("div",{'class':'caas-body'}).text)
+        except:
+            # published_today.append("")
+            article.append(float("nan"))
+    # data 
+    # data.div
+    user_dataset["Published today"] = published_today
+    user_dataset["Article"] = article
     return user_dataset
--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ import sys
 import yfinance as yf
 sys.path.insert(0,"/Users/williammckeon/Desktop/news-screener/src")
 from updating_data import identitfing_tickers
-if __names =="__main__":
+from filter import user_filter
+if __name__ =="__main__":
     tickers = pd.read_csv("ticker_info.csv")
-    
+    final_dataset = user_filter(tickers)
+    print(final_dataset.head(10))
 
